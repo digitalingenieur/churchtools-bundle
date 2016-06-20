19c77fea11a7dd211f6b4e51d8bf3e79b647c40f
Samuel Heer <samuel.heer@diging.de>
2016-06-20 21:23:59 +0200

    init commit

---
+++ src/ContaoChurchtoolsBundle.php
@@ -0,0 +1,22 @@
+<?php
+
+/*
+ * This file is part of Contao Churchtools Bundle.
+ *
+ * Copyright (c) 2016 Samuel Heer
+ *
+ * @license LGPL-3.0+
+ */
+
+namespace Diging\ChurchtoolsBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+/**
+ * Configures the Contao Churchtools bundle.
+ *
+ * @author Leo Feyer <https://github.com/leofeyer>
+ */
+class ContaoChurchtoolsBundle extends Bundle
+{
+}
